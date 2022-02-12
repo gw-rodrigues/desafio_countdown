4d0124ebec583b30bd57630a55e1b8614ac5845f
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-02-12 23:46:59 +0100

    add modal popup, add form modal

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 (()=>{
 
+    //               COUNTDOWN SECTION
+    // VARS
     const countdown = {
         date_now: new Date('2022 01 01 00:00:00'),
         date_end: new Date('2022 01 01 00:00:00'),
@@ -8,9 +10,11 @@
         e_hours: document.getElementById("hours"),
         e_minutes: document.getElementById("minutes"),
         e_seconds: document.getElementById("seconds"),
-        
+        e_start_button: document.getElementById('start'),
+        e_input_days: document.getElementById('days'),
+        e_input_time: document.getElementById('time')
     }
-
+    // DECREMENT
     function remaning_time() {
         
         countdown.date_end.setMilliseconds(-1000)
@@ -35,8 +39,9 @@
         }
        
     }
-
+    // START
     function start_countdown (days, hours, minutes, seconds){
+        countdown.date_end = new Date('2022 01 01 00:00:00')
         if(!countdown.timer_is_on){
             countdown.date_end.setDate(countdown.date_end.getDate() + days)
             countdown.date_end.setHours(hours)
@@ -46,6 +51,26 @@
         }
     }
 
+    //CALL
     start_countdown(0,0,0,10)
+
+    countdown.e_input_days.value = '0';
+    countdown.e_input_time.value = '00:00:10';
+
+    function getData(){
+        console.log(countdown.e_input_days.value)
+    }
+
+    countdown.e_start_button.addEventListener('click', getData)
+
+
+    //                      MODAL SECTION
+    //TOGGLE CLASS
+    function toogleModal() {
+        document.getElementById('modal').classList.toggle('opened')
+    }
+
+    document.getElementById('modal-open-button').addEventListener('click', toogleModal)
+    document.getElementById('modal-close-button').addEventListener('click', toogleModal)
 
 })();
